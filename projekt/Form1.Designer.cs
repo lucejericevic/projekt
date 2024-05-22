4d489e6e39561f697baeead79963b670a72edea7
lucejericevic <luce.jericevic@skole.hr>
2024-05-22 17:09:14 +0200

    izbornik

--- projekt/Form1.Designer.cs
+++ projekt/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace projekt
 {
-    partial class Form1
+    partial class Izbornik
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Izbornik));
+            this.label_dobroDosli = new System.Windows.Forms.Label();
+            this.labelizaberiteIgricu = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonTreseta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_dobroDosli
+            // 
+            this.label_dobroDosli.AutoSize = true;
+            this.label_dobroDosli.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_dobroDosli.Location = new System.Drawing.Point(28, 32);
+            this.label_dobroDosli.Name = "label_dobroDosli";
+            this.label_dobroDosli.Size = new System.Drawing.Size(292, 55);
+            this.label_dobroDosli.TabIndex = 0;
+            this.label_dobroDosli.Text = "Dobro došli!";
+            this.label_dobroDosli.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelizaberiteIgricu
+            // 
+            this.labelizaberiteIgricu.AutoSize = true;
+            this.labelizaberiteIgricu.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelizaberiteIgricu.Location = new System.Drawing.Point(32, 105);
+            this.labelizaberiteIgricu.Name = "labelizaberiteIgricu";
+            this.labelizaberiteIgricu.Size = new System.Drawing.Size(213, 31);
+            this.labelizaberiteIgricu.TabIndex = 1;
+            this.labelizaberiteIgricu.Text = "Odaberite igricu:";
+            this.labelizaberiteIgricu.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(352, 78);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.Location = new System.Drawing.Point(376, 189);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(352, 78);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Križić Kružić";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // buttonTreseta
+            // 
+            this.buttonTreseta.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonTreseta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonTreseta.BackgroundImage")));
+            this.buttonTreseta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonTreseta.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonTreseta.Location = new System.Drawing.Point(376, 105);
+            this.buttonTreseta.Name = "buttonTreseta";
+            this.buttonTreseta.Size = new System.Drawing.Size(352, 78);
+            this.buttonTreseta.TabIndex = 2;
+            this.buttonTreseta.Text = "Trešeta";
+            this.buttonTreseta.UseVisualStyleBackColor = false;
+            this.buttonTreseta.Click += new System.EventHandler(this.buttonTreseta_Click);
+            // 
+            // Izbornik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonTreseta);
+            this.Controls.Add(this.labelizaberiteIgricu);
+            this.Controls.Add(this.label_dobroDosli);
+            this.Name = "Izbornik";
+            this.Text = "Izbornik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_dobroDosli;
+        private System.Windows.Forms.Label labelizaberiteIgricu;
+        private System.Windows.Forms.Button buttonTreseta;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- projekt/Form1.cs
+++ projekt/Form1.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace projekt
 {
-    public partial class Form1 : Form
+    public partial class Izbornik : Form
     {
-        public Form1()
+        public Izbornik()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTreseta_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
