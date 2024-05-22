e12dddb0be37abadb20d6456347f018c18bfa7cc
lucejericevic <luce.jericevic@skole.hr>
2024-05-22 18:17:14 +0200

    dodala san formu za krizic kruzic i dizajn (triba jos napravit funkcionalosti i spojit s botunon u izborniku)

---
+++ projekt/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekt
+{
+    public partial class KrizicKruzic : Form
+    {
+        public KrizicKruzic()
+        {
+            InitializeComponent();
+        }
+
+
+        //ovo ce se dogodit kad igrac pritisne bilo koje polje 
+        private void IgracClick(object sender, EventArgs e)
+        {
+
+        }
+
+        //ovo je timer
+        private void AIPotez(object sender, EventArgs e)
+        {
+
+        }
+
+        //ovo je za resetirat igricu
+        private void ResetirajIgru(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
