6056b983da317b20928da4da93e36d990001163b
lukajugov <luka.jugov@skole.hr>
2024-05-28 21:02:20 +0200

    zapoceta klasa za ucitavanje pojedine karte

--- projekt/Karta.cs
+++ projekt/Karta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,19 @@ namespace projekt
 {
     internal class Karta
     {
+        string[] Zog = ["K", "S", "B", "D"];
+        int[] Broj = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        double[] Vrijednost = [0, 0.33, 1];
+        //path = ...
+        
 
+        private void slikaKarte(string Zog, int Broj)
+        {
+           v
+        }
+
+
+
+        
     }
 }
